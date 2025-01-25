45e9e6765e44316bdebd45b7d869a15143016ed9
ayush <ayush91003@gmail.com>
2025-01-25 20:07:57 +0530

    Firebase connected successfully

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:thinknest/features/auth/screens/login_screen.dart';
+import 'package:thinknest/firebase_options.dart';
 import 'package:thinknest/theme/pallete.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  debugPrint('Firebase Initialized');
   runApp(const MyApp());
 }
 
